5aeffc0e5b78c7703291b38d2426907ae669ab0f
“shunsuke-ue” <shunsuke.ue@gmail.com>
2023-07-23 08:04:11 +0900

    deviseのインストール

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v1 do
-      get 'test/index'
+      resources :test, only: %i[index]
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
